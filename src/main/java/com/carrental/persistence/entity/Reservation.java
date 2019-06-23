62292161932b41ab81610e0192c964f1e0e5ad80
Szymon Patora <szymonopatora@gmail.com>
2019-06-23 13:22:49 +0200

    Reservattionentity

---
+++ src/main/java/com/carrental/persistence/entity/Reservation.java
@@ -0,0 +1,27 @@
+package com.carrental.persistence.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+
+@Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private Client client;
+    private Car car;
+    private LocalDate dateRental;
+    private LocalDate dateReturn;
+
+}
