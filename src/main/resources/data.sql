fe7c14294cf2a1066772a6f9472a1a102a65730f
Grażyna Krzyżanowska <liralen@op.pl>
2019-06-29 12:39:26 +0200

    sql branch

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+insert into branch (id,city,country,street,zip_code) values
+(1,'Lubin','Poland','Wierzbowa','59-300'),
+(2,'Gdynia','Poland','Kwiatowa','65-832'),
+(3,'Wrocław','Poland','Fabryczna','78-217'),
+(4,'Poznań','Poland','Maszynowa','61-623'),
+(5,'Torzym','Poland','Kolorowa','66-600'),
+(6,'Kraków','Poland','Smocza','50-831');
