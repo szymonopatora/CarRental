09e65f97d89028d0b4344f2e7109c0f103f5b441
Szymon Patora <szymonopatora@gmail.com>
2019-07-06 11:57:05 +0200

    AdminController added

--- src/main/java/com/carrental/controller/AdminController.java
+++ src/main/java/com/carrental/controller/AdminController.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class IndexController {
+public class AdminController {
 
-    @GetMapping({"", "/", "/index"})
-    public String getIndex() {
-        return "index";
+    @GetMapping("/administration")
+    public String getAdminPage() {
+        return "administration";
     }
 }
